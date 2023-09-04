ae9fa074628dec7453f2c9ccbf3760cdd53c0049
Nandhini2410 <81728339+Nandhini2410@users.noreply.github.com>
2023-09-04 18:01:05 +0530

    Add files via upload

---
+++ EmployeeID.sql
@@ -0,0 +1,45 @@
+Create Table EmployeeID
+(
+  Dependent_Id Int,
+  First_Name Varchar(50) Not Null,
+  Last_Name Varchar(50) Not Null,
+  Relationship Char(5),
+  EmployeeID Int,
+  )
+
+ Insert the data into the EmployeeID table
+
+  INSERT INTO EmployeeID (Dependent_Id,First_Name,Last_Name,Relationship,EmployeeID) Values (1,'Penelope','Gietz','Child',206)
+  INSERT INTO EmployeeID (Dependent_Id,First_Name,Last_Name,Relationship,EmployeeID) Values (2,'Nick','Higgins','Child',205)
+  INSERT INTO EmployeeID (Dependent_ID,First_Name,Last_Name,Relationship,EmployeeID) Values (3,'Ed','Whalen','Child',200)
+  INSERT INTO EmployeeID (Dependent_ID,First_Name,Last_Name,Relationship,EmployeeID) Values (4,'Jennifer','King','Child',100)
+  INSERT INTO EmployeeID (Dependent_ID,First_Name,Last_Name,Relationship,EmployeeID) Values (5, 'Johnny','Kochhar','Child',101)
+  INSERT INTO EmployeeID (Dependent_ID,First_Name,Last_Name,Relationship,EmployeeID) Values (6,'Bette','De Haan','Child',102)
+  INSERT INTO EmployeeID (Dependent_ID,First_Name,Last_Name,Relationship,EmployeeID) Values (7,'Grace','Faviet','Child',109)
+  INSERT INTO EmployeeID (Dependent_ID,First_Name,Last_Name,Relationship,EmployeeID) Values (8,'Matthew','Chen','Chlid',110)
+  INSERT INTO EmployeeID (Dependent_ID,First_Name,Last_Name,Relationship,EmployeeID) Values (9,'Joe','Sciarra','Child',111)
+  INSERT INTO EmployeeID (Dependent_ID,First_Name,Last_Name,Relationship,EmployeeID) Values (10,'Christian','Urman','Chlid',112)
+  INSERT INTO EmployeeID (Dependent_ID,First_Name,Last_Name,Relationship,EmployeeID) Values (11,'Zero','Popp','Child',113)
+  INSERT INTO EmployeeID (Dependent_ID,First_Name,Last_Name,Relationship,EmployeeID) Values (12,'Karl','Greenberg','Child',108)
+  INSERT INTO EmployeeID (Dependent_ID,First_Name,Last_Name,Relationship,EmployeeID) Values (13,'Uma','Mavris','Child',203)
+  INSERT INTO EmployeeID (Dependent_ID,First_Name,Last_Name,Relationship,EmployeeID) Values (14,'Vivien','Hunold','Chlid',103)
+  INSERT INTO EmployeeID (Dependent_ID,First_Name,Last_Name,Relationship,EmployeeID) Values (15,'Cuba','Ernst','Chlid',104)
+  INSERT INTO EmployeeID (Dependent_ID,First_Name,Last_Name,Relationship,EmployeeID) Values (16,'Fred','Austin','Chlid',105)
+  INSERT INTO EmployeeID (Dependent_ID,First_Name,Last_Name,Relationship,EmployeeID) Values (17,'Helen','Pataballa','Child',106)
+  INSERT INTO EmployeeID (Dependent_ID,First_Name,Last_Name,Relationship,EmployeeID) Values (18,'Dan','Lorentz','Child',107)
+  INSERT INTO EmployeeID (Dependent_ID,First_Name,Last_Name,Relationship,EmployeeID) Values (19,'Bob','Hartstein','Child',201)
+  INSERT INTO EmployeeID (Dependent_ID,First_Name,Last_Name,Relationship,EmployeeID) Values (20,'Lucille','Fay','Child',202)
+  INSERT INTO EmployeeID (Dependent_ID,First_Name,Last_Name,Relationship,EmployeeID) Values (21,'Kirsten','Baer','Child',204)
+  INSERT INTO EmployeeID (Dependent_ID,First_Name,Last_Name,Relationship,EmployeeID) Values (22,'Elvis','Khoo','Child',115)
+  INSERT INTO EmployeeID (Dependent_ID,First_Name,Last_Name,Relationship,EmployeeID) Values (23,'Sandra','Baida','Child',116)
+  INSERT INTO EmployeeID (Dependent_ID,First_Name,Last_Name,Relationship,EmployeeID) Values (24,'Cameron','Tobias','Child',117)
+  INSERT INTO EmployeeID (Dependent_ID,First_Name,Last_Name,Relationship,EmployeeID) Values (25,'Kevin','Himuro','Child',118)
+  INSERT INTO EmployeeID (Dependent_ID,First_Name,Last_Name,Relationship,EmployeeID) Values (26,'Rip','Colmenares','Child',119)
+  INSERT INTO EmployeeID (Dependent_ID,First_Name,Last_Name,Relationship,EmployeeID) Values (27,'Julia','Raphaely','Child',114)
+  INSERT INTO EmployeeID (Dependent_ID,First_Name,Last_Name,Relationship,EmployeeID) Values (28,'Woody','Russell','Child',145)
+  INSERT INTO EmployeeID (Dependent_ID,First_Name,Last_Name,Relationship,EmployeeID) Values (29,'Alec','Partners','Child',146)
+  INSERT INTO EmployeeID (Dependent_ID,First_Name,Last_Name,Relationship,EmployeeID) Values (30,'Sandra','Taylor','Child',176)
+
+
+
+  SELECT Dependent_Id,First_Name,Last_Name,Relationship,EmployeeID from EmployeeID
